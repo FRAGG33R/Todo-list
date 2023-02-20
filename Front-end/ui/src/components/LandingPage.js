d5f453399082c48236d7cd28174d7532c0ccbadf
FRAGG33R <aissambarchil75@gmail.com>
2023-02-20 15:52:49 +0100

    adjust paragragh size

--- Front-end/ui/src/components/LandingPage.js
+++ Front-end/ui/src/components/LandingPage.js
@@ -13,7 +13,7 @@ export default function LandingPage() {
 			<div className="z-10 3xl:w-3/5 xs:w-full max-h-3/5 flex items-center space-y-80 flex-col  text-[#1c5d51]">
 				<div className="w-11/12 flex items-center justify-center flex-col space-y-8">
 					<div className='4xl:text-6xl xl:text-5xl sm:text-4xl xs:text-2xl font-bold text-center tracking-wider'>KEEP YOUR LIFE UNDER CONTROL</div>
-					<div className='2xl:text-2xl lg:text-xl md:text-lg sm:text-md xs:text-md font-normal sm:w-8/12 xs:w-full text-center '>Make every day more productive with our intuitive to-do list app. Stay organized, set priorities, and achieve your goals. Sign up now and start getting things done.</div>
+					<div className='2xl:text-2xl lg:text-xl md:text-lg sm:text-md xs:text-md font-light w-9/12 text-center '>Make every day more productive with our intuitive to-do list app. Stay organized, set priorities, and achieve your goals. Sign up now and start getting things done.</div>
 				</div>
 				<a href="#_" class="px-5 py-2.5 relative rounded-full group text-white font-medium inline-block">
 					<span class="absolute top-0 left-0 w-full h-full rounded-full opacity-50 filter blur-sm bg-gradient-to-tr from-[#32D8AC] to-[#0F5A56]"></span>
