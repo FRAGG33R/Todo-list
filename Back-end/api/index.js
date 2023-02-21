088647eb7ff9a692949e5bd4348fa63dd385711b
FRAGG33R <aissambarchil75@gmail.com>
2023-02-21 22:55:38 +0100

    commit before fixing the issue of authentication

--- Back-end/api/index.js
+++ Back-end/api/index.js
@@ -6,20 +6,12 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.post('/app', async (req, res) => {
+app.post('/app', (req, res) => {
 	console.log(req.body);
-	const user = await prisma.user.create(
-		{
-			data :{
-				name : req.body.name,
-				email: req.body.email
-			}
-		}
-	)
 	res.status(200);
 })
 app.get('/', (req, res) => {
 
 })
 
-app.listen(3000);
+app.listen(300);
