55e191678a70e4f07baf44937e296413ca62b50d
FRAGG33R <aissambarchil75@gmail.com>
2023-02-23 20:27:34 +0100

    add icon  and avatar

--- Front-end/ui/src/components/SideBar.js
+++ Front-end/ui/src/components/SideBar.js
@@ -3,10 +3,10 @@ import App from "./App"
 import { useAuth0 } from '@auth0/auth0-react';
 import { useEffect, useInsertionEffect, useState } from "react";
 import { Navigate } from 'react-router-dom';
+import { IconNotes } from '@tabler/icons-react';
 
 export default function SideBar() {
 	const { user, isAuthenticated, isLoading  } = useAuth0();
-	const [counter, setCounter] = useState(3);
 
 	useEffect(() => {
 
@@ -22,49 +22,13 @@ export default function SideBar() {
 			<div class="relative w-full pt-4 flex items-center justify-center ">
 				<img class="w-10 h-10 rounded-full" src={user?.picture} alt="" />
 			</div>
-				<ul class="flex flex-col space-y-4 py-6">
-				<li>
-					<a href="#_" class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-white">
-					<span class="inline-flex items-center justify-center h-12 w-12 text-lg "><i class="bx bx-home"></i></span>
-					<span class="text-sm font-medium">lorem ipsume</span>
-					</a>
-				</li>
-				<li>
-					<a href="#_" class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-white">
-					<span class="inline-flex items-center justify-center h-12 w-12 text-lg "><i class="bx bx-music"></i></span>
-					<span class="text-sm font-medium">lorem ipsume</span>
-					</a>
-				</li>
-				<li>
-					<a href="#_" class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-white">
-					<span class="inline-flex items-center justify-center h-12 w-12 text-lg "><i class="bx bx-drink"></i></span>
-					<span class="text-sm font-medium">lorem ipsume</span>
-					</a>
-				</li>
-				<li>
-					<a href="#_" class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-white">
-					<span class="inline-flex items-center justify-center h-12 w-12 text-lg "><i class="bx bx-shopping-bag"></i></span>
-					<span class="text-sm font-medium">lorem ipsume</span>
-					</a>
-				</li>
-				<li>
-					<a href="#_" class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-white">
-					<span class="inline-flex items-center justify-center h-12 w-12 text-lg "><i class="bx bx-chat"></i></span>
-					<span class="text-sm font-medium">lorem ipsume</span>
-					</a>
-				</li>
-				<li>
-					<a href="#__" class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-white">
-					<span class="inline-flex items-center justify-center h-12 w-12 text-lg "><i class="bx bx-user"></i></span>
-					<span class="text-sm font-medium">lorem ipsume</span>
-					</a>
-				</li>
-				<li>
-					<a href="#_" class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-white">
-					<span class="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i class="bx bx-log-out"></i></span>
-					<span class="text-sm font-medium">lorem ipsume</span>
-					</a>
-				</li>
+				<ul class="flex pl-3 flex-col space-y-4 py-4">
+					<li>
+						<a href="#_" class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-white">
+							<IconNotes height={18} color="white" stroke-width="1.25" />
+							<span class="text-sm font-medium pl-1">asdjlkf</span>
+						</a>
+					</li>
 				</ul>
 			</div>
 			<App />
